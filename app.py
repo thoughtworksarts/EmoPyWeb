627ce4909eda18e0e55e249a02c7e36402af2770
MR Ngo <mrngo@thoughtworks.com>
2019-04-02 11:04:25 -0400

    doesn't predict emotion when no face is detected

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def predict():
     # Passing the frame to the predictor
     emotion = ''
     with graph.as_default():
-        emotion = model.predict_from_ndarray(image_np)
+        face = crop_to_first_face(image_np)
+        emotion = model.predict_from_ndarray(image_np) if face is not None else 'no face detected'
     return emotion
 
 def data_uri_to_cv2_img(uri):
@@ -36,6 +37,20 @@ def data_uri_to_cv2_img(uri):
     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
+
+def crop_to_first_face(image):
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+    if len(faces) > 0:
+        (x, y, w, h) = faces[0]
+        return gray[y:y+h, x:x+w]
+    return None
 
 if __name__ == '__main__':
     app.run()
