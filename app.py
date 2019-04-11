c064bab0e3e8c3a3ff216c7adcf1d242cfd8f2c9
MR Ngo <mrngo@thoughtworks.com>
2019-04-11 10:25:10 -0400

    remove cropping logic as it's now in EmoPy

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
+
+from EmoPy import FERModel
+from EmoPy.src.face_detection import FaceDetector
 from flask import Flask, render_template, request
 import tensorflow as tf
 import keras
-from EmoPy.src.fermodel import FERModel
 
 import base64
 import cv2
@@ -13,7 +15,7 @@ import numpy as np
 target_emotions = ['calm', 'anger', 'happiness']
 
 graph = tf.get_default_graph()
-model = FERModel(target_emotions, verbose=False)
+model = FERModel(target_emotions, verbose=False, face_detector=FaceDetector('haarcascade_frontalface_default.xml'))
 
 # Initialize application
 app = Flask(__name__)
@@ -22,15 +24,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     image_np = data_uri_to_cv2_img(request.values['image'])
     # Passing the frame to the predictor
-    emotion = ''
     with graph.as_default():
-        face = crop_to_first_face(image_np)
-        emotion = model.predict_from_ndarray(image_np) if face is not None else 'no face detected'
+        emotion = model.predict_from_ndarray(image_np)
     return emotion
+
 
 def data_uri_to_cv2_img(uri):
     encoded_data = uri.split(',')[1]
@@ -38,19 +40,6 @@ def data_uri_to_cv2_img(uri):
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
 
-def crop_to_first_face(image):
-    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
-    if len(faces) > 0:
-        (x, y, w, h) = faces[0]
-        return gray[y:y+h, x:x+w]
-    return None
 
 if __name__ == '__main__':
     app.run()
