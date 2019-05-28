c81bec4f92a3b54c9fdea89140f1d1563752fba3
Emilio Escobedo <emilio.escobedo@thoughtworks.com>
2019-05-28 16:48:33 -0400

    Countdown 3 seconds after button click before taking the photo

--- static/index.js
+++ static/index.js
@@ -102,70 +102,88 @@ function hasGetUserMedia() {
     )
   }
 
+
+var timer = null;
+
   function createAndSavePolaroid() {
-    var polaroidCanvas = document.createElement('canvas');
-    var polaroidCtx = polaroidCanvas.getContext('2d');
-    var polaroidDiv = document.getElementById('polaroid');
-    var polaroidFooterDiv = document.getElementById('polaroid-footer');
-    var emotionDiv = document.getElementById('emotion');
-    var dateDiv = document.getElementById('date');
-    const polaroidPadding = 30; // space between edge of polaroid and start of webcam
-    polaroidCanvas.width = polaroidDiv.offsetWidth;
-    polaroidCanvas.height = polaroidDiv.offsetHeight;
-    var polaroidFooterDivOffset = polaroidDiv.offsetHeight - polaroidFooterDiv.offsetHeight;
-    polaroidCtx.fillStyle = window.getComputedStyle(polaroidDiv).getPropertyValue('background-color');
-    polaroidCtx.fillRect(0, 0, polaroidCanvas.width, polaroidCanvas.height);
-    polaroidCtx.font = window.getComputedStyle(emotionDiv).getPropertyValue('font');
-    polaroidCtx.fillStyle = window.getComputedStyle(emotionDiv).getPropertyValue('color');
-    var emotionText = emotionDiv.textContent;
-    var emotionTextWidth = polaroidCtx.measureText(emotionText).width;
-    polaroidCtx.fillText(emotionText, (polaroidCanvas.width/2) - (emotionTextWidth/2), polaroidFooterDivOffset + 20);
-    polaroidCtx.font = window.getComputedStyle(dateDiv).getPropertyValue('font');
-    polaroidCtx.fillStyle = window.getComputedStyle(dateDiv).getPropertyValue('color');
-    var dateText = dateDiv.textContent;
-    var dateTextWidth = polaroidCtx.measureText(dateText).width;
-    polaroidCtx.fillText(dateText, (polaroidCanvas.width/2) - (dateTextWidth/2), polaroidFooterDivOffset + 60);
-    var logo = new Image();
-    logo.src = './static/twa-logo.png'
-    logo.onload = function() {
-      polaroidCtx.drawImage(logo, (polaroidCanvas.width/2) - ((this.width/3)/2), polaroidFooterDivOffset + 75, this.width/3, this.height/3);
-      polaroidCtx.drawImage(canvas, polaroidPadding, polaroidPadding);
-      polaroidCtx.drawImage(emotionOverlay, polaroidPadding, polaroidPadding);
-      let imagePng = polaroidCanvas.toDataURL('image/png');
-      let imageKey = uuidv4() + '.png';
-      s3.upload({
-        Key: imageKey,
-        Body: dataURItoBlob(imagePng),
-      }, function(err, data) {
-        if (err) {
-          return alert('There was an error uploading your photo: ', err, data);
-        }
-        alert('Successfully uploaded photo.');
-        
-        s3.getSignedUrl('getObject', {
-            Key: imageKey,
-            Expires: 86400
-        }, function(err, url) {
-            if (err) return err;
-            console.log(url);
-            $.ajax({
-                type: "POST",
-                url: "/shorten-url",
-                data:{
-                    longUrl: url
+    var timeto = 3; // time in seconds to capture
+    var countdown = $("#timer").html(timeto);
+    if (timer !== null) {
+        // some logic here if autoshoot in progress and user click button again
+    } else {
+        $("#autoshootmsg").show();
+        timer = window.setInterval(function() {
+            timeto--;
+            countdown.html(timeto);
+            if (timeto == 0) {
+                window.clearInterval(timer);
+                timer = null;
+                $("#autoshootmsg").hide();
+                var polaroidCanvas = document.createElement('canvas');
+                var polaroidCtx = polaroidCanvas.getContext('2d');
+                var polaroidDiv = document.getElementById('polaroid');
+                var polaroidFooterDiv = document.getElementById('polaroid-footer');
+                var emotionDiv = document.getElementById('emotion');
+                var dateDiv = document.getElementById('date');
+                const polaroidPadding = 30; // space between edge of polaroid and start of webcam
+                polaroidCanvas.width = polaroidDiv.offsetWidth;
+                polaroidCanvas.height = polaroidDiv.offsetHeight;
+                var polaroidFooterDivOffset = polaroidDiv.offsetHeight - polaroidFooterDiv.offsetHeight;
+                polaroidCtx.fillStyle = window.getComputedStyle(polaroidDiv).getPropertyValue('background-color');
+                polaroidCtx.fillRect(0, 0, polaroidCanvas.width, polaroidCanvas.height);
+                polaroidCtx.font = window.getComputedStyle(emotionDiv).getPropertyValue('font');
+                polaroidCtx.fillStyle = window.getComputedStyle(emotionDiv).getPropertyValue('color');
+                var emotionText = emotionDiv.textContent;
+                var emotionTextWidth = polaroidCtx.measureText(emotionText).width;
+                polaroidCtx.fillText(emotionText, (polaroidCanvas.width/2) - (emotionTextWidth/2), polaroidFooterDivOffset + 20);
+                polaroidCtx.font = window.getComputedStyle(dateDiv).getPropertyValue('font');
+                polaroidCtx.fillStyle = window.getComputedStyle(dateDiv).getPropertyValue('color');
+                var dateText = dateDiv.textContent;
+                var dateTextWidth = polaroidCtx.measureText(dateText).width;
+                polaroidCtx.fillText(dateText, (polaroidCanvas.width/2) - (dateTextWidth/2), polaroidFooterDivOffset + 60);
+                var logo = new Image();
+                logo.src = './static/twa-logo.png'
+                logo.onload = function() {
+                polaroidCtx.drawImage(logo, (polaroidCanvas.width/2) - ((this.width/3)/2), polaroidFooterDivOffset + 75, this.width/3, this.height/3);
+                polaroidCtx.drawImage(canvas, polaroidPadding, polaroidPadding);
+                polaroidCtx.drawImage(emotionOverlay, polaroidPadding, polaroidPadding);
+                let imagePng = polaroidCanvas.toDataURL('image/png');
+                let imageKey = uuidv4() + '.png';
+                s3.upload({
+                    Key: imageKey,
+                    Body: dataURItoBlob(imagePng),
+                }, function(err, data) {
+                    if (err) {
+                    return alert('There was an error uploading your photo: ', err, data);
+                    }
+                    
+                    s3.getSignedUrl('getObject', {
+                        Key: imageKey,
+                        Expires: 86400
+                    }, function(err, url) {
+                        if (err) return err;
+                        $.ajax({
+                            type: "POST",
+                            url: "/shorten-url",
+                            data:{
+                                longUrl: url
+                            }
+                        }).done(function(res) {
+                            QRCode.toCanvas(document.getElementById('qrcode-img'), res, function (error) {
+                                if (error) console.error(error)
+                            });
+                        });
+                    });
+                });
+                if('{{twitter}}' == 'True') 
+                    shareImage(imagePng, document.getElementById('twitterUsername').value);
                 }
-            }).done(function(res) {
-                QRCode.toCanvas(document.getElementById('qrcode-img'), res, function (error) {
-                    if (error) console.error(error)
-                  });
-            });
-        });
-      });
-      if('{{twitter}}' == 'True') 
-        shareImage(imagePng, document.getElementById('twitterUsername').value);
-      
-    }
+            };
+        }, 1000);
+    };
   }
+
+  
 
   function shareImage(imageURL, twitterUsername) {
     $.ajax({
