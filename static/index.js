fc3fbcbfb26581918e574db6fde427e41c2677fc
Emilio Escobedo <emilio.escobedo@thoughtworks.com>
2019-07-30 11:59:06 -0400

    Fix counters, I think

--- static/index.js
+++ static/index.js
@@ -6,7 +6,7 @@ $.ajax({
     conf = JSON.parse(res);
     AWS.config.region = conf.region;
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: conf.identityPoolId
+      IdentityPoolId: conf.identityPoolId
     });
     s3 = new AWS.S3({
     apiVersion: '2006-03-01',
@@ -23,58 +23,151 @@ function displayDate() {
 }
 displayDate();
 
+
 function hasGetUserMedia() {
-    return !!(navigator.mediaDevices &&
-      navigator.mediaDevices.getUserMedia);
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+if (hasGetUserMedia()) {
+} else {
+  alert('getUserMedia() is not supported by your browser');
+}
+
+var constraints = {
+  video: true
+};
+
+var video = document.querySelector('video');
+
+function handleSuccess(stream) {
+  window.stream = stream; // only to make stream available to console
+  video.srcObject = stream;
+  predict();
+}
+
+function handleError(error) {
+  console.error(error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).
+  then(handleSuccess).catch(handleError);
+
+const canvas = document.getElementById('canvas');
+const emotionOverlay = document.getElementById('emotionOverlay');
+emotionCtx = emotionOverlay.getContext('2d');
+
+let isEmotion = true;
+function createAndSavePolaroid() {
+  newPhoto();
+  trigger.html('<span style="font-size:100px;">'+timeto+'</span>');
+  takePhoto().prop('disabled', true);
+  photoCountdown = window.setInterval(function() {
+    timeto--;
+    trigger.html('<span style="font-size:100px;">'+timeto+'</span>');
+    if (timeto == 0) {
+      isEmotion = false;
+      window.clearInterval(photoCountdown);
+      timeto = 3;
+    };
+  }, 1000);
+}
+
+function predict() {
+  if(!isEmotion) return actuallyTakePhoto();
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+
+  // Other browsers will fall back to image/png
+  dataURL = canvas.toDataURL();
+  $.ajax({
+    type: "POST",
+    url: "/predict",
+    data:{
+      image: dataURL
+    }
+  }).done(function(res) {
+    displayEmotion(res);
+  }).always(predict);
+};
+
+function actuallyTakePhoto() {
+  trigger.hide();
+  video.pause();
+  isEmotion = false;
+  var polaroidCanvas = document.createElement('canvas');
+  var polaroidCtx = polaroidCanvas.getContext('2d');
+  var polaroidDiv = document.getElementById('polaroid');
+  var polaroidFooterDiv = document.getElementById('polaroid-footer');
+  var emotionDiv = document.getElementById('emotion');
+  var dateDiv = document.getElementById('date');
+  const polaroidPadding = 30; // space between edge of polaroid and start of webcam
+  polaroidCanvas.width = polaroidDiv.offsetWidth;
+  polaroidCanvas.height = polaroidDiv.offsetHeight;
+  var polaroidFooterDivOffset = polaroidDiv.offsetHeight - polaroidFooterDiv.offsetHeight;
+  polaroidCtx.fillStyle = window.getComputedStyle(polaroidDiv).getPropertyValue('background-color');
+  polaroidCtx.fillRect(0, 0, polaroidCanvas.width, polaroidCanvas.height);
+  polaroidCtx.font = window.getComputedStyle(emotionDiv).getPropertyValue('font');
+  polaroidCtx.fillStyle = window.getComputedStyle(emotionDiv).getPropertyValue('color');
+  var emotionText = emotionDiv.textContent;
+  var emotionTextWidth = polaroidCtx.measureText(emotionText).width;
+  polaroidCtx.fillText(emotionText, (polaroidCanvas.width/2) - (emotionTextWidth/2), polaroidFooterDivOffset + 20);
+  polaroidCtx.font = window.getComputedStyle(dateDiv).getPropertyValue('font');
+  polaroidCtx.fillStyle = window.getComputedStyle(dateDiv).getPropertyValue('color');
+  var dateText = dateDiv.textContent;
+  var dateTextWidth = polaroidCtx.measureText(dateText).width;
+  polaroidCtx.fillText(dateText, (polaroidCanvas.width/2) - (dateTextWidth/2), polaroidFooterDivOffset + 55);
+  var locationTextWidth = polaroidCtx.measureText(document.getElementById('location').textContent).width;
+  polaroidCtx.fillText(document.getElementById('location').textContent, (polaroidCanvas.width/2) - (locationTextWidth/2), polaroidFooterDivOffset + 75);
+  var logo = new Image();
+  logo.src = './static/twa-logo.png'
+  logo.onload = function() {
+    polaroidCtx.drawImage(logo, (polaroidCanvas.width/2) - ((this.width/3)/2), polaroidFooterDivOffset + 75, this.width/3, this.height/3);
+    polaroidCtx.drawImage(canvas, polaroidPadding, polaroidPadding);
+    polaroidCtx.drawImage(emotionOverlay, polaroidPadding, polaroidPadding);
+    let imagePng = polaroidCanvas.toDataURL('image/png');
+    let imageKey = uuidv4() + '.png';
+    s3.upload({
+      Key: imageKey,
+      Body: dataURItoBlob(imagePng),
+      ContentType: 'image/png'
+    }, function(err, data) {
+      if (err) return alert('There was an error uploading your photo: ', err, data);
+      s3.getSignedUrl('getObject', {
+        Key: imageKey,
+        Expires: 86400
+      }, function(err, url) {
+        if (err) return err;
+        $.ajax({
+          type: "POST",
+          url: "/shorten-url",
+          data:{
+              longUrl: url
+          }
+        }).done(function(res) {
+          console.log(res);
+          qrcode.makeCode(res);
+          $('#bitly').html(res.substring(7));
+          qrContainer.show();
+          takePhoto().prop('disabled', false);
+          var clearQrButton = $('#qr-disappear');
+          let qrtimeto = 20;
+          clearQrButton.html("QR code will disappear in " + qrtimeto + " seconds");
+          qrCountdown = window.setInterval(function() {
+            qrtimeto--;
+            clearQrButton.html("QR code will disappear in " + qrtimeto + " seconds");
+            if (qrtimeto == 0) {
+              newPhoto();
+            }
+          }, 1000);
+        });
+      });
+    });
+    if('{{twitter}}' == 'True') 
+        shareImage(imagePng, document.getElementById('twitterUsername').value);
   }
-
-  if (hasGetUserMedia()) {
-  } else {
-    alert('getUserMedia() is not supported by your browser');
-  }
-
-  var constraints = {
-    video: true
-  };
-
-  var video = document.querySelector('video');
-
-  function handleSuccess(stream) {
-    window.stream = stream; // only to make stream available to console
-    video.srcObject = stream;
-    predict();
-  }
-
-  function handleError(error) {
-    console.error(error);
-  }
-
-  navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
-
-  const canvas = document.getElementById('canvas');
-  const emotionOverlay = document.getElementById('emotionOverlay');
-  emotionCtx = emotionOverlay.getContext('2d');
-  let isEmotion = true;
-
-  function predict() {
-    if(!isEmotion) return actuallyTakePhoto();
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0);
-
-    // Other browsers will fall back to image/png
-    dataURL = canvas.toDataURL();
-    $.ajax({
-      type: "POST",
-      url: "/predict",
-      data:{
-        image: dataURL
-      }
-    }).done(function(res) {
-      displayEmotion(res);
-    }).always(predict);
-  };
+}
 
   function displayEmotion(emotionPrediction){
     if(isEmotion){
@@ -141,6 +234,7 @@ function newPhoto() {
   photoCountdown = null;
   qrCountdown = null;
   qrContainer.hide();
+  qrContainer.src = '#';
   qrDescription().show();
 }
 
@@ -151,101 +245,7 @@ function qrDescription() {
 function takePhoto() {
   return $('#take-photo');
 }
-
-function actuallyTakePhoto() {
-  takePhoto().prop('disabled', false);
-      trigger.hide();
-      video.pause();
-      isEmotion = false;
-      window.clearInterval(photoCountdown);
-      timeto = 3;
-      var polaroidCanvas = document.createElement('canvas');
-      var polaroidCtx = polaroidCanvas.getContext('2d');
-      var polaroidDiv = document.getElementById('polaroid');
-      var polaroidFooterDiv = document.getElementById('polaroid-footer');
-      var emotionDiv = document.getElementById('emotion');
-      var dateDiv = document.getElementById('date');
-      const polaroidPadding = 30; // space between edge of polaroid and start of webcam
-      polaroidCanvas.width = polaroidDiv.offsetWidth;
-      polaroidCanvas.height = polaroidDiv.offsetHeight;
-      var polaroidFooterDivOffset = polaroidDiv.offsetHeight - polaroidFooterDiv.offsetHeight;
-      polaroidCtx.fillStyle = window.getComputedStyle(polaroidDiv).getPropertyValue('background-color');
-      polaroidCtx.fillRect(0, 0, polaroidCanvas.width, polaroidCanvas.height);
-      polaroidCtx.font = window.getComputedStyle(emotionDiv).getPropertyValue('font');
-      polaroidCtx.fillStyle = window.getComputedStyle(emotionDiv).getPropertyValue('color');
-      var emotionText = emotionDiv.textContent;
-      var emotionTextWidth = polaroidCtx.measureText(emotionText).width;
-      polaroidCtx.fillText(emotionText, (polaroidCanvas.width/2) - (emotionTextWidth/2), polaroidFooterDivOffset + 20);
-      polaroidCtx.font = window.getComputedStyle(dateDiv).getPropertyValue('font');
-      polaroidCtx.fillStyle = window.getComputedStyle(dateDiv).getPropertyValue('color');
-      var dateText = dateDiv.textContent;
-      var dateTextWidth = polaroidCtx.measureText(dateText).width;
-      polaroidCtx.fillText(dateText, (polaroidCanvas.width/2) - (dateTextWidth/2), polaroidFooterDivOffset + 55);
-      var locationTextWidth = polaroidCtx.measureText(document.getElementById('location').textContent).width;
-      polaroidCtx.fillText(document.getElementById('location').textContent, (polaroidCanvas.width/2) - (locationTextWidth/2), polaroidFooterDivOffset + 75);
-      var logo = new Image();
-      logo.src = './static/twa-logo.png'
-      logo.onload = function() {
-      polaroidCtx.drawImage(logo, (polaroidCanvas.width/2) - ((this.width/3)/2), polaroidFooterDivOffset + 75, this.width/3, this.height/3);
-      polaroidCtx.drawImage(canvas, polaroidPadding, polaroidPadding);
-      polaroidCtx.drawImage(emotionOverlay, polaroidPadding, polaroidPadding);
-      let imagePng = polaroidCanvas.toDataURL('image/png');
-      let imageKey = uuidv4() + '.png';
-      s3.upload({
-        Key: imageKey,
-        Body: dataURItoBlob(imagePng),
-        ContentType: 'image/png'
-      }, function(err, data) {
-        if (err) return alert('There was an error uploading your photo: ', err, data);
-        s3.getSignedUrl('getObject', {
-          Key: imageKey,
-          Expires: 86400
-        }, function(err, url) {
-          if (err) return err;
-          $.ajax({
-            type: "POST",
-            url: "/shorten-url",
-            data:{
-                longUrl: url
-            }
-          }).done(function(res) {
-            console.log(res);
-            qrcode.makeCode(res);
-            $('#bitly').html(res.substring(7));
-            qrContainer.show();
-            var clearQrButton = $('#qr-disappear');
-            var qrtimeto = 20;
-            clearQrButton.html("QR code will disappear in " + qrtimeto + " seconds");
-            qrCountdown = window.setInterval(function() {
-              qrtimeto--;
-              clearQrButton.html("QR code will disappear in " + qrtimeto + " seconds");
-              if (qrtimeto == 0) {
-                newPhoto();
-              }
-            }, 1000);
-          });
-        });
-      });
-      if('{{twitter}}' == 'True') 
-          shareImage(imagePng, document.getElementById('twitterUsername').value);
-      }
-}
   
-function createAndSavePolaroid() {
-  newPhoto();
-  trigger.html('<span style="font-size:100px;">'+timeto+'</span>');
-  takePhoto().prop('disabled', true);
-  photoCountdown = window.setInterval(function() {
-    timeto--;
-    trigger.html('<span style="font-size:100px;">'+timeto+'</span>');
-    if (timeto == 0) {
-      isEmotion = false;
-    };
-  }, 1000);
-}
-
-  
-
   function shareImage(imageURL, twitterUsername) {
     $.ajax({
       type: "POST",
